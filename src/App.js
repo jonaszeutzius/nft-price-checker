4f5edd5030057cdaa42f8f512ef2931da71caf2e
vboxuser <vboxuser@blockspan-ubuntu.myguest.virtualbox.org>
2023-06-16 11:03:10 -0600

    error handling

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   const [contractAddress, setContractAddress] = useState('');
   const [blockchain, setBlockchain] = useState('eth-main');
   const [nftData, setNftData] = useState(null);
+  const [error, setError] = useState(null); // New state for error message
 
   const fetchNFTData = async () => {
     const options = {
@@ -15,8 +16,13 @@ const App = () => {
       headers: {accept: 'application/json', 'X-API-KEY': '2jhzbqIWanB8puiqySBIWJVf6Ovp7oPW'}
     };
 
-    const response = await axios.request(options);
-    setNftData(response.data);
+    try {
+      const response = await axios.request(options);
+      setNftData(response.data);
+      setError(null); // Reset error state if successful
+    } catch (error) {
+      setError('NFT not found'); // Set error message if API call fails
+    }
   };
 
   const handleBlockchainChange = (event) => {
@@ -26,8 +32,16 @@ const App = () => {
   return (
     <div className="App">
       <h1 className='title'>NFT Price Checker</h1>
-      <p>Input NFT token ID and contract address below to see recent transaction price.</p>
+      <p>Select a chain and input NFT token ID and contract address below to see recent transaction price.</p>
       <div className="inputContainer">
+        <select name='blockchain' value={blockchain} onChange={handleBlockchainChange}>
+          <option value="eth-main">eth-main</option>
+          <option value="arbitrum-main">arbitrum-main</option>
+          <option value="optimism-main">optimism-main</option>
+          <option value="poly-main">poly-main</option>
+          <option value="bsc-main">bsc-main</option>
+          <option value="eth-goerli">eth-goerli</option>
+        </select>
         <input 
           type="text"
           placeholder="Contract Address"
@@ -40,32 +54,27 @@ const App = () => {
           value={tokenId}
           onChange={e => setTokenId(e.target.value)}
         />
-        <select name='blockchain' value={blockchain} onChange={handleBlockchainChange}>
-          <option value="eth-main">eth-main</option>
-          <option value="arbitrum-main">arbitrum-main</option>
-          <option value="optimism-main">optimism-main</option>
-          <option value="poly-main">poly-main</option>
-          <option value="bsc-main">bsc-main</option>
-          <option value="eth-goerli">eth-goerli</option>
-          {console.log('This is the blockchain:' + blockchain)}
-        </select>
         <button onClick={fetchNFTData}>Check Price</button>
       </div>
       
-      {nftData && (
-      <div className="nftData">
-        <h2>{nftData.name}</h2>
-        <div className="imageContainer">
-          <img className="image" src={nftData.cached_images.medium_500_500} alt={nftData.name} />
-        </div>
-        <p className='message'>
-          Id: {nftData.id} | 
-          Token Name: {nftData.token_name} | 
-          {(nftData.rarity_rank)? `Rarity: ${nftData.rarity_rank} | ` : " "}
-          Recent Price: ${parseFloat(nftData.recent_price.price_usd).toFixed(2)} {`(${nftData.recent_price.price} ${nftData.recent_price.price_currency})`}
-        </p>
-      </div>
-      )}
+      {error ?
+        <p>{error}</p> :
+        nftData && (
+          <div className="nftData">
+            <h2>{nftData.name}</h2>
+            <div className="imageContainer">
+              <img className="image" src={nftData.cached_images.medium_500_500} alt={nftData.name} />
+            </div>
+            <p className='message'>
+              Id: {nftData.id} | 
+              Token Name: {nftData.token_name} | 
+              {(nftData.rarity_rank)? `Rarity: ${nftData.rarity_rank} | ` : " "}
+              Recent Price: ${parseFloat(nftData.recent_price.price_usd).toFixed(2)} {`(${nftData.recent_price.price} ${nftData.recent_price.price_currency})`}
+            </p>
+          </div>
+        )
+      }
+    
     </div>
   );
 }
